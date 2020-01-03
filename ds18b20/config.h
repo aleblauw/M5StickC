2c16de8e47b1e5c22bf3790ace61d59a98bd8125
Ale <ale.blauw@kpn.com>
2020-01-03 14:45:36 +0100

    Updated ignore file

--- ds18b20/config.h
+++ ds18b20/config.h
@@ -2,7 +2,7 @@
 #define WIFIPASSWORD "password"
 
 #define POST_URL "URL"
-#define AUTH_TOKEN "TOKEN"
+#define AUTH_TOKEN "Token"
 
 #define POST_URL_ADAFRUIT "https://io.adafruit.com/api/v2/<USERNAME>/feeds/<FEEDNAME>/data"
 #define ADAFRUIT_HEADER "X-AIO-Key", "<AIO_KEY>" 
