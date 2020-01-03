3ceca79dfda0f9d3048dba4bf9711a18db128d37
Ale <ale.blauw@kpn.com>
2020-01-03 14:37:33 +0100

    Updated ignore file

--- ds18b20/config.h
+++ ds18b20/config.h
@@ -4,7 +4,6 @@
 #define POST_URL "URL"
 #define AUTH_TOKEN "Token"
 
-
 #define POST_URL_ADAFRUIT "https://io.adafruit.com/api/v2/<USERNAME>/feeds/<FEEDNAME>/data"
 #define ADAFRUIT_HEADER "X-AIO-Key", "<AIO_KEY>" 
 #define ADAFRUIT_HEADER2 "Content-Type", "application/x-www-form-urlencoded"
